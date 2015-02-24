d55014bc7312c09180c78cbcbd396059ca5291cf
Masato Tsukagoshi <masato.tsukagoshi@O-02935-MAC.local>
2015-02-24 19:58:41 +0900

    add pusher

---
+++ Assets/pusher.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var origin : Vector3;
+function Awake () {
+	origin = rigidbody.position;
+}
+
+function Update () {
+	var offs = Vector3(0, 0, Mathf.Sin(Time.time));
+	rigidbody.MovePosition(origin +  offs);
+}
